8f86006e0dae96e39894fc43c9fcb6a646d550d8
maximus <bernardmayowaa@gmail.com>
2025-01-27 14:50:09 +0100

    started

---
+++ 02-variables/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	var isLoggedIn bool = true
+	fmt.Println(isLoggedIn)
+	fmt.Printf("Varibale is of type: %T \n", isLoggedIn)
+
+	var anotherTest string
+	fmt.Println(anotherTest)
+	fmt.Printf("Varibale is of type: %T \n", anotherTest)
+
+	congrats :="happy birthday"
+	print(congrats)
+}
